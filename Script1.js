646056a92a322fc2ec204c991ac8c6f5d2c5edf4
Chrispy6 <116905484+Chrispy6@users.noreply.github.com>
2023-12-18 05:21:43 -0700

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -3,21 +3,3 @@ d3.select('#showPdfLink').on('click', function () {
             var pdfContainer = d3.select('#pic1');
             pdfContainer.style('visibility', pdfContainer.style('visibility') === 'hidden' ? 'visible' : 'hidden');
         });
-document.addEventListener("DOMContentLoaded", function () {
-    var textElement = d3.select("#picdes2");
-    var pictureElement = d3.select("#pic2");
-
-    function updateOpacity() {
-        var scrollPosition = window.scrollY;
-        var windowHeight = window.innerHeight;
-
-        var textVisibility = 1 - Math.max(0, Math.min(1, (textElement.node().offsetTop - scrollPosition) / windowHeight));
-        var pictureVisibility = 1 - Math.max(0, Math.min(1, (pictureElement.node().offsetTop - scrollPosition) / windowHeight));
-
-
-        textElement.style("opacity", textVisibility);
-        pictureElement.style("opacity", pictureVisibility);
-    }
-    window.addEventListener("scroll", updateOpacity);
-    updateOpacity();
-});
